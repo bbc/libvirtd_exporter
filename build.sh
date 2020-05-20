921f9928515352d5450a62ac57096d2583ab117d
James Gibson <james.gibson@bbc.co.uk>
2020-05-20 16:42:27 +0100

    Create build script which exports the build artefacts
    Change-Id: Ifa1458a6b17484d378ec14b4cba2e0493b722f7b

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Small script to build libvirtd-exporter using docker and export the resulting binary
+
+docker build -t libvirtd-exporter .
+
+# Start container with result
+docker run  --entrypoint sh -itd --name libvirtd-exporter libvirtd-exporter
+
+# Extract build atifact to host
+docker cp libvirtd-exporter:/libvirtd_exporter .
+
+docker stop libvirtd-exporter
+docker rm -f libvirtd-exporter
